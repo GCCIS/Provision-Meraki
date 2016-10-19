ddc90c539eec69be9162997c58647f46e8672f89
jmcnatt <jmcnatt@live.com>
2016-10-19 14:06:34 -0400

    Initial script layout.

---
+++ Provision-Meraki.ps1
@@ -0,0 +1,27 @@
+<#
+	.SYNOPSIS
+	
+	.DESCRIPTION
+	
+	.PARAMETER Name
+
+    .EXAMPLE
+	
+	.LINK
+
+#>
+param (
+    [Parameter(Position = 0, Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+    [ValidateNotNullOrEmpty]
+    [String] $Key
+)
+
+begin
+{
+
+}
+
+process
+{
+    
+}
