3a7e29f5bd054fe35f2ed759eec948883576b4ec
jmcnatt <jmcnatt@live.com>
2016-10-19 14:08:36 -0400

    Added organization parameter and declared vars

--- Provision-Meraki.ps1
+++ Provision-Meraki.ps1
@@ -13,11 +13,17 @@
 param (
     [Parameter(Position = 0, Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
     [ValidateNotNullOrEmpty]
-    [String] $Key
+    [String] $Key,
+
+    [Parameter(Position = 1, Mandatory = $true, ValueFromPipeline = $false, ValueFromPipelineByPropertyName = $false)]
+    [ValidateNotNullOrEmpty]
+    [String] $Organization
 )
 
 begin
 {
+    $Api = 'v0'
+    $BaseUrl = 'https://dashboard.meraki.com'
 
 }
 
