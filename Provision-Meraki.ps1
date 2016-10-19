0a3847c2a9eea943645bdb90ac43f606a0080911
jmcnatt <jmcnatt@live.com>
2016-10-19 14:28:56 -0400

    Added invoke statement in New-MerakiNetwork

--- Provision-Meraki.ps1
+++ Provision-Meraki.ps1
@@ -24,6 +24,22 @@ begin
 {
     $Api = 'v0'
     $BaseUrl = 'https://dashboard.meraki.com'
+    $Headers = @{"X-Cisco-Meraki-API-Key"="$key"}
+
+    function New-MerakiNetwork
+    {
+        param (
+            [Parameter(Position = 0, Mandatory = $true)]
+            [String] $Name
+        )
+
+        Invoke-WebRequest -Headers $Headers -Method Post -Uri "$BaseUrl/$Api/$Organization/networks"
+    }
+
+    function Remove-MerakiNetwork
+    {
+
+    }
 
 }
 
